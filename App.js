0bbc089af5ffed87edae3aa03ceef13c3aad9935
neon2k21 <roman_2001@icloud.com>
2023-12-06 17:53:27 +0500

    test

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const showconsole = ()=>{
+  console.log('privet')
+}
 
 export default function App() {
+var text1 ='demo'
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="bg-[#1b81c1] w-full"
+    style={{height:hp(50),padding:wp(10)}}>
+      <TouchableOpacity
+      onPress={()=>{showconsole()}}
+      className="w-full h-full bg-white">
+        <Text className="uppercase" style={{fontSize:wp(10)}} onPress={()=>{showconsole}} >
+       TEXT
+        </Text>
+      </TouchableOpacity>
+        <Text className="uppercase" style={{fontSize:wp(10)}} >
+        {text1}
+        </Text>
+        
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
